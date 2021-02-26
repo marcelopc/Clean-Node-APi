fbec471122f8d8d988013a67726036d288cf748a
Marcelo <marcelopc04@gmail.com>
2021-02-26 00:02:02 -0300

    move success http response to a helper method

--- src/presentation/controllers/signup/signup.ts
+++ src/presentation/controllers/signup/signup.ts
@@ -2,7 +2,7 @@ import {
   HttpRequest, HttpResponse, Controller, EmailValidator, AddAccount, AddAccountModel,
 } from './sigup-protocols';
 import { MissingParamError, InvalidParamError } from '../../errors';
-import { badRequest, serverError } from '../../helpers/http-helper';
+import { badRequest, serverError, ok } from '../../helpers/http-helper';
 
 export class SignUpController implements Controller {
   private readonly emailValidator: EmailValidator
@@ -38,7 +38,7 @@ export class SignUpController implements Controller {
 
       const account = this.addAccount.add({ name, email, password });
 
-      return { statusCode: 200, body: account };
+      return ok(account);
     } catch (error) {
       return serverError();
     }
