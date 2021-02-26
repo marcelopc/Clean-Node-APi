e55481d629b95483ad54a689876637015ea83310
Marcelo <marcelopc04@gmail.com>
2021-02-25 23:48:43 -0300

    move all sigup protocols to a single file

---
+++ src/presentation/controllers/signup/sigup-protocols.ts
@@ -0,0 +1,6 @@
+import { format } from 'path';
+
+export * from '../../protocols';
+export * from '../../protocols/email-validator';
+export * from '../../../domain/usecases/add-account';
+export * from '../../../domain/models/account';
