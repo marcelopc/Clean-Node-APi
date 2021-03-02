672af9aeb823886afcfcc6c920dea88e5bf07454
Marcelo <marcelopc04@gmail.com>
2021-03-02 00:01:21 -0300

    ensure AccountMongoRepository on success

--- src/infra/db/mongodb/helprs/mongo-helper.ts
+++ src/infra/db/mongodb/helprs/mongo-helper.ts
@@ -1,14 +1,21 @@
-import { MongoClient } from 'mongodb';
+import { MongoClient, Collection } from 'mongodb';
 
+console.log(MongoClient);
 export const MongoHelper = {
   client: null as MongoClient,
+
   async connect(uri: string):Promise<void> {
     this.client = await MongoClient.connect(uri, {
       useNewUrlParser: true,
       useUnifiedTopology: true,
     });
   },
+
   async disconnect():Promise<void> {
     await this.client.close();
+  },
+
+  getCollection(name:string): Collection {
+    return this.client.db().collection(name);
   },
 };
