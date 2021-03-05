c2c1aca234239b1fe37d545d8d8a45404358fc38
Marcelo <marcelopc04@gmail.com>
2021-03-05 08:49:42 -0300

    connect to mongodb before app listem

--- src/main/server.ts
+++ src/main/server.ts
@@ -1,3 +1,7 @@
-import app from './config/app';
+import { MongoHelper } from '../infra/db/mongodb/helprs/mongo-helper';
+import env from './config/env';
 
-app.listen(8000, () => console.log('Server running at http://localhost8000'));
+MongoHelper.connect(env.mongoUrl).then(async () => {
+  const app = (await import('./config/app')).default;
+  app.listen(env.port, () => console.log(`Server running at http://localhost:${env.port}`));
+}).catch(console.error);
