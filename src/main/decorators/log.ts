057294f34beff62db03cf543b7e1f81614c41139
Marcelo <marcelopc04@gmail.com>
2021-03-07 17:57:16 -0300

    make server error return the error stack

--- src/main/decorators/log.ts
+++ src/main/decorators/log.ts
@@ -9,7 +9,9 @@ export class LogControllerDecorator implements Controller {
 
     async handle(httpRequest: HttpRequest): Promise<HttpResponse> {
       const httpResponse = await this.controller.handle(httpRequest);
-      //   if (httpResponse.statusCode === 500) {}
+      //   if (httpResponse.statusCode === 500) {
+
+      //   }
 
       return httpResponse;
     }
